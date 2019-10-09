b6e4d4a4c4a87e9c1e374b93a20c193246b459b6
metalnodeug <metalnodeug@gmail.com>
2019-10-09 23:54:02 +0200

    First commit

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,10 +9,68 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = "2"
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double((Int(numberOfPeople) ?? 0))
+        let totalPerPerson = amountWithTip / peopleCount
+        
+        return totalPerPerson
+    }
+    
+    var amountWithTip: Double {
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = (orderAmount * tipSelection) / 100
+        let grandTotale = orderAmount + tipValue
+        
+        return grandTotale
+    }
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    TextField("Number of people", text: $numberOfPeople)
+                        .keyboardType(.decimalPad)
+                }
+                
+                
+                Section {
+                    Text("How much tip do you want to leave?")
+                    
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section{
+                    Text("Total Amount with tip value")
+                    Text("$\(amountWithTip, specifier: "%.2f")")
+                }
+                
+                Section{
+                    Text("Amount per person")
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+            }
+            .navigationBarTitle("WeSplit")
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
